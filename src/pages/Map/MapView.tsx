2c7539f330ccef88ebe002db24fe90fbb0aab2ce
Haldun Yildiz <haldun313@gmail.com>
2020-03-28 16:48:28 +0300

    -Integrae @paraboly/pwc-map to main map

--- src/pages/Map/MapView.tsx
+++ src/pages/Map/MapView.tsx
@@ -1,5 +1,10 @@
-import React from "react";
+/* eslint-disable @typescript-eslint/no-namespace */
+import React, { useRef } from "react";
 import Stories from 'react-insta-stories';
+import "mapbox-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
+import "@paraboly/pwc-map";
+
 import {
   IonHeader,
   IonToolbar,
@@ -13,7 +18,16 @@ import { Location } from "../../models/Location";
 import { connect } from "../../data/connect";
 import * as selectors from "../../data/selectors";
 import "./MapView.scss";
-//import STORIES from './Stories.mock';
+
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      'pwc-map': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
+      'pwc-map-geojson-layer': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
+      'pwc-map-corona': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
+    }
+  }
+}
 
 const STORIES: any = [{
   header: {
@@ -43,6 +57,12 @@ interface DispatchProps { }
 interface MapViewProps extends OwnProps, StateProps, DispatchProps { }
 
 const MapView: React.FC<MapViewProps> = ({ locations, mapCenter }) => {
+  const pwcMap = useRef<HTMLDivElement>(null);
+
+  if (pwcMap.current) {
+    startMap(pwcMap.current);
+  }
+
   return (
     <IonPage id="map-view">
       <IonHeader>
@@ -62,10 +82,20 @@ const MapView: React.FC<MapViewProps> = ({ locations, mapCenter }) => {
             loop={true}
           />
         </div>
+        <pwc-map ref={pwcMap}>
+          <pwc-map-geojson-layer />
+          <pwc-map-corona />
+        </pwc-map>
       </IonContent>
     </IonPage>
   );
 };
+
+
+const startMap = (pwcMap: HTMLElement) => {
+  pwcMap.classList.add("show-map");
+  console.log("MapStarted: ", pwcMap);
+}
 
 export default connect<OwnProps, StateProps, DispatchProps>({
   mapStateToProps: state => ({
